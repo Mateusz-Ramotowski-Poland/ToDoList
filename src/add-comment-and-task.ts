48754088dbc1d44a48c3d71e6da30fc7fd576ae5
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2022-08-09 14:49:49 +0200

    Sorting of tasks works.

--- src/add-comment-and-task.ts
+++ src/add-comment-and-task.ts
@@ -1,19 +1,14 @@
-export interface TaskArray {
+export interface Task {
   title: string | undefined;
   dueDate: string;
   id: number;
 }
 
-const btnAddTask: HTMLButtonElement | null = document.querySelector(
-  "[name='manage-task__btn-add-task']"
-);
-export const listTask: HTMLUListElement | null =
-  document.querySelector(".tasks");
-export const taskArray1: TaskArray[] = [];
+const btnAddTask: HTMLButtonElement | null = document.querySelector(".manage-task__btn-add-task");
+export const listTask: HTMLUListElement | null = document.querySelector(".tasks");
+export const taskArray: Task[] = [];
 
-function checkInputValidity(
-  input: HTMLInputElement | Element | null | undefined
-): boolean {
+function checkInputValidity(input: HTMLInputElement | null | undefined): boolean {
   if (!input?.checkValidity()) {
     input?.reportValidity();
     return false;
@@ -37,11 +32,7 @@ function getYearMonthDayString(): string {
   return actualDate;
 }
 
-export function createNewTaskString(
-  title: string | undefined,
-  date: string,
-  id: number
-): string {
+export function createNewTaskString(title: string | undefined, date: string, id: number): string {
   return `
 <div class="task" data-id=${id}>
   <div class="task__main">
@@ -71,7 +62,7 @@ export function createNewTaskString(
       required
       minlength="4"
     />
-    <button name="task__btn-add-comment" type="submit">Add comment</button>
+    <button class="task__btn-add-comment" type="submit">Add comment</button>
   </form>
   <div class="comments"></div>
 </div>
@@ -81,19 +72,13 @@ export function createNewTaskString(
 function addNewTask(event: Event): void {
   event.preventDefault();
 
-  const textInputTask: HTMLInputElement | null = document.querySelector(
-    "[name='manage-task__textInput']"
-  );
+  const textInputTask: HTMLInputElement | null = document.querySelector("[name='manage-task__textInput']");
   if (!checkInputValidity(textInputTask)) return;
 
   const id: number = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-  const newTask: string = createNewTaskString(
-    textInputTask?.value,
-    getYearMonthDayString(),
-    id
-  );
+  const newTask: string = createNewTaskString(textInputTask?.value, getYearMonthDayString(), id);
 
-  taskArray1.push({
+  taskArray.push({
     title: textInputTask?.value,
     dueDate: getYearMonthDayString(),
     id: id,
@@ -105,24 +90,16 @@ function addNewTask(event: Event): void {
 btnAddTask?.addEventListener("click", addNewTask);
 
 function addNewComment(event: Event): void {
-  if (
-    !(
-      (event?.target as Element)?.getAttribute("name") ===
-      "task__btn-add-comment"
-    )
-  ) {
+  if (!(event?.target as Element)?.classList.contains("task__btn-add-comment")) {
     return;
   }
 
   event.preventDefault();
 
   const currentTask = (event.target as Element).closest(".task");
-  const inputAuthor: HTMLInputElement | null =
-    currentTask?.querySelector("[name='input-author']") || null;
-  const inputComment: HTMLInputElement | null =
-    currentTask?.querySelector("[name='input-comment']") || null;
-  const comments: HTMLDivElement | null =
-    currentTask?.querySelector(".comments") || null;
+  const inputAuthor: HTMLInputElement | null = currentTask?.querySelector("[name='input-author']") || null;
+  const inputComment: HTMLInputElement | null = currentTask?.querySelector("[name='input-comment']") || null;
+  const comments: HTMLDivElement | null = currentTask?.querySelector(".comments") || null;
 
   if (!checkInputValidity(inputAuthor)) return;
   if (!checkInputValidity(inputComment)) return;
@@ -131,7 +108,7 @@ function addNewComment(event: Event): void {
   const actualDate: string = `${getYearMonthDayString()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
   const newComment: string = `
   <p class="comment">
-    ${actualDate}. ${inputAuthor?.value}: ${inputComment?.value}
+    ${actualDate}. ${inputAuthor?.value}:${inputComment?.value}
   </p>
     `;
 
--- src/change-dueDate.ts
+++ src/change-dueDate.ts
@@ -1,5 +1,16 @@
-import { taskArray1, TaskArray, listTask } from "./add-comment-and-task";
+import { taskArray, Task, listTask } from "./add-comment-and-task";
 
-function changeTaskDueDate() {}
+function changeTaskDueDate(event: Event) {
+  if (!((event.target as Element)?.getAttribute("name") === "task__date")) {
+    return;
+  }
 
-listTask?.addEventListener("click", changeTaskDueDate);
+  const chosenTask: Element | null = (event?.target as Element)?.closest(".task");
+  const indexOfChangedTask: number = taskArray.findIndex(
+    (el) => el.id.toString() === (chosenTask as HTMLElement)?.dataset.id
+  );
+
+  taskArray[indexOfChangedTask].dueDate = (event.target as HTMLInputElement)?.value;
+}
+
+listTask?.addEventListener("input", changeTaskDueDate);
