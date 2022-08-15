e841f720879843b0cf3f93873bcda4730522389b
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2022-08-15 13:56:39 +0200

    Remove code duplicationfrom sortTasks function. Added global variable for sorting button state. Now filtering works with sorting. Now I import only one main.ts file inside index.html

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import { Task } from "./interfaces";
-import { addAllEventListeners } from "./add-all-event-listeners";
+import { addAllEventListeners, taskArray } from "./add-all-event-listeners";
 import { renderTasks } from "./render-tasks";
 
 export const listTask: HTMLUListElement | null = document.querySelector(".tasks");
 export const btnAddTask: HTMLButtonElement | null = document.querySelector(".manage-task__btn-add-task");
-export const btnSortArray = document.querySelector(".manage-task__sort-tasks");
+export const btnSortArray: HTMLButtonElement | null = document.querySelector(".manage-task__sort-tasks");
 
-export const taskArray: Task[] = JSON.parse(localStorage.getItem("tasks") ?? "[]");
 export const btnFilter: HTMLButtonElement | null = document.querySelector(".manage-task__btn-filter");
 
 renderTasks(taskArray);
